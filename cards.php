78f17747ee4a1db5ebbcf6cd1b0e854a98402ad5
fredstp <freal1802@gmail.com>
2020-03-26 11:02:28 +0100

    updated cards.php

--- cards.php
+++ cards.php
@@ -1,3 +1,41 @@
 <?php
+$cards = [
+            [
+                'image'  => 'burdigala-I.jpg',
+                'title'   => 'Burdigala I', 
+                'description'    => 'Bordeaux - Royan Ardeo, mihi credite, Patres conscripti (id quod vosmet de me existimatis et facitis ipsi) incredibili quodam amore patriae, qui me amor et subvenire olim impendentibus periculis maximis cum dimicatione capitis, et rursum, cum omnia tela undique esse intenta in patriam viderem, subire coegit atque excipere unum pro universis',
+            ],
+            [
+                'image'  => 'burdigala-II.jpg',
+                'title'   => 'Burdigala II', 
+                'description'    => 'Bordeaux - Royan Ardeo, mihi credite, Patres conscripti (id quod vosmet de me existimatis et facitis ipsi) incredibili quodam amore patriae, qui me amor et subvenire olim impendentibus periculis maximis cum dimicatione capitis, et rursum, cum omnia tela undique esse intenta in patriam viderem, subire coegit atque excipere unum pro universis',
+            ],
+            [
+                'image'  => 'titanic-II.jpg',
+                'title'   => 'Titanic II', 
+                'description'    => 'Bordeaux - Royan Ardeo, mihi credite, Patres conscripti (id quod vosmet de me existimatis et facitis ipsi) incredibili quodam amore patriae, qui me amor et subvenire olim impendentibus periculis maximis cum dimicatione capitis, et rursum, cum omnia tela undique esse intenta in patriam viderem, subire coegit atque excipere unum pro universis',
+            ],
+            
+   
 
+        ];
+foreach ($cards as $card) {
+     echo '<div class="cont">';
+     echo   '<div class="container">';
+     echo       '<div class="card_Circuit_Pair">';
+     echo         '<div class="card_Circuit_Pair_img">';
+     echo              '<img class="boat" src="./img/'. $card['image'] . '" alt="' . $card['title'] . '">';
+     echo          '</div>';
+     echo           '<div class="card_Circuit_Pair_Description">';
+     echo               '<div class="card_Circuit_Pair_Description_Text">';
+     echo                 '<h2>' . $card['title'] . '</h2>';
+     echo                   '<p>' . $card['description'] . '</p>';
+     echo              '</div>';
+     echo              '<div class="card_Circuit_Pair_Description_button">';
+     echo                   '<a href="resa.html">';
+     echo                  '<button class="button"  type="button"> Réserver </button></a>';
+     echo               '</div>';
+     echo          '</div>';
+     echo      '</div>';
+};
 ?>
--- circuits.php
+++ circuits.php
@@ -3,63 +3,7 @@
 <?php include("head.php"); ?>
 <body>
     <?php include("header.php"); ?>
-    <div class="cont">
-        <div class="container">
-            <div class="card_Circuit_Pair">
-                <div class=" card_Circuit_Pair_img">
-                    <img class="boat" src="./img/burdigala-I.jpg" alt="burdigala">
-                </div>
-                <div class="card_Circuit_Pair_Description">
-                    <div class="card_Circuit_Pair_Description_Text">
-                        <h2> Burdigala I</h2>
-                        <p>Bordeaux - Royan Ardeo, mihi credite, Patres conscripti (id quod vosmet de me existimatis et facitis ipsi) incredibili quodam amore patriae, qui me amor et subvenire olim impendentibus periculis maximis cum dimicatione capitis, et rursum, cum omnia tela undique esse intenta in patriam viderem, subire coegit atque excipere unum pro universis</p>
-                    </div>
-                    <div class="card_Circuit_Pair_Description_button">
-                        <a href="resa.html">
-                        <button class="button"  type="button"> Réserver </button></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="cont">
-        <div class="container">
-            <div class="card_Circuit_Impair">
-                <div class="card_Circuit_Impair_Description">
-                    <div class="card_Circuit_Impair_Description_Text">
-                        <h2> Burdigala II</h2>
-                        <p>Bordeaux - Blaye Ardeo, mihi credite, Patres conscripti (id quod vosmet de me existimatis et facitis ipsi) incredibili quodam amore patriae, qui me amor et subvenire olim impendentibus periculis maximis cum dimicatione capitis, et rursum, cum omnia tela undique esse intenta in patriam viderem, subire coegit atque excipere unum pro universis</p>
-                    </div>
-                    <div class="card_Circuit_Impair_Description_button">
-                        <a href="resa.html">
-                        <button class="button" type="button"> Réserver </button></a>
-                    </div>
-                 </div>
-                <div class=" card_Circuit_Impair_img">
-                    <img class="boat" src="./img/burdigala-II.jpg" alt="burdigalaII">
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="cont">
-        <div class="container">
-            <div class="card_Circuit_Pair">
-                <div class=" card_Circuit_Pair_img">
-                    <img class="boat" src="./img/titanic-II.jpg" alt="titanicII">
-                </div>
-                <div class="card_Circuit_Pair_Description">
-                    <div class="card_Circuit_Pair_Description_Text">
-                        <h2> Titanic II</h2>
-                        <p class="text">Transatlantic Ardeo, mihi credite, Patres conscripti (id quod vosmet de me existimatis et facitis ipsi) incredibili quodam amore patriae, qui me amor et subvenire olim impendentibus periculis maximis cum dimicatione capitis, et rursum, cum omnia tela undique esse intenta in patriam viderem, subire coegit atque excipere unum pro universis</p>
-                    </div>
-                    <div class="card_Circuit_Pair_Description_button">
-                        <a href="resa.html">
-                        <button class="button" type="button"> Réserver </button></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <?php include ("cards.php"); ?>
     <?php include ("footer.php"); ?>
 </body>
 </html>
